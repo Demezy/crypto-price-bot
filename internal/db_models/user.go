2a2038edc56a549dddf4b8e4d5cbe7f3fc709bdd
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-21 01:49:21 +0300

    feat: implement user model

---
+++ internal/db_models/user.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"fmt"
+	"simple-go-telegram-bot/internal/consts"
+	"time"
+)
+
+type User struct {
+	ID           uint   `gorm:"primaryKey"`
+	TelegramId   string `gorm:"unique"`
+	QueriyNumber int
+	LastQuery    time.Time
+	FirstQuery   time.Time
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("{id: %d, telegram_id: %s, query_number: %d, last_query: %s, first_query: %s}",
+		u.ID,
+		u.TelegramId,
+		u.QueriyNumber,
+		u.LastQuery.Format(consts.TimeFormat),
+		u.FirstQuery.Format(consts.TimeFormat),
+	)
+}
