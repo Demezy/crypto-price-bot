290ef6f565f487cf5b6c04abb29962d9a22d86bd
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-21 01:50:06 +0300

    feat: db initialization

---
+++ internal/db/db.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	models "simple-go-telegram-bot/internal/db_models"
+	"simple-go-telegram-bot/internal/types"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB(host, user, password, port string) (db types.DB, err error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
+		host,
+		user,
+		password,
+		port,
+	)
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func MigrateDB(db types.DB) {
+	db.AutoMigrate(&models.User{})
+}
+
+func ConnectDBFromEnv() (db types.DB, err error) {
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	port := os.Getenv("POSTGRES_PORT")
+	if host == "" || port == "" {
+		return nil, errors.New("Undefined database host or port")
+	}
+
+	return ConnectDB(host, user, password, port)
+}
