be0bb2a5d25e7ba62f62ca3f89e2aed4785668a6
Demezy <38487319+Demezy@users.noreply.github.com>
2023-03-21 02:00:08 +0300

    refactor: move api urls to consts

---
+++ internal/consts/urls.go
@@ -0,0 +1,6 @@
+package consts
+
+const (
+	BaseApi       = "https://api.coingecko.com/api/v3/"
+	CurrencyPrice = BaseApi + "coins/%s?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false"
+)
